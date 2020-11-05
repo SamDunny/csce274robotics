9dfca6e73290f1b43141a96446931ddf78fe2a7b
SamDunny <samdunny.edu11@gmail.com>
2020-11-05 18:52:10 -0500

    polishing circle.py

--- packages/project3/src/circle.py
+++ packages/project3/src/circle.py
@@ -4,13 +4,13 @@ import rospy
 from duckietown_msgs.msg import Twist2DStamped
 from time import sleep
 
-class circle():
+def circle():
     pub = rospy.Publisher('car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
     rospy.init_node('project_3_circle')
-    msg = Twist2DStamped(header=None, v = '0.1', omega = '0.3')
+    msg = Twist2DStamped(header=None, v = 0.1, omega = 0.3)
     pub.publish(msg)
-    sleep(10) # robot runs for 10 seconds
-    msg = Twist2DStamped(header = None, v = '0', omega = '0') # stop robot
+    sleep(30) # robot runs for 10 seconds
+    msg = Twist2DStamped(header = None, v = 0, omega = 0) # stop robot
     pub.publish(msg)
     
     

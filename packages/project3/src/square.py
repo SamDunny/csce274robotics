d0388a6374ad4081e1d20845717114a38e0bccc0
SamDunny <samdunny.edu11@gmail.com>
2020-11-05 20:46:33 -0500

    polishing square.py

--- packages/project3/src/square.py
+++ packages/project3/src/square.py
@@ -8,31 +8,13 @@ def square():
     pub = rospy.Publisher('car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
     rospy.init_node('project_3_square')
     msg1 = Twist2DStamped(header=None, v = 0.3, omega = 0.0)
-    msg2 = Twist2DStamped(header=None, v = 0.0, omega = 0.5)
-
-    # first side and turn
-   # pub.publish(msg1)
-   # sleep(7)
-   # pub.publish(msg2)
-   # sleep(4)
-
-    # second side and turn
-   # pub.publish(msg1)
-   # sleep(7)
-   # pub.publish(msg2)
-   # sleep(4)
-
-    # third side and turn
-   # pub.publish(msg1)
-   # sleep(7)
-   # pub.publish(msg2)
-   # sleep(4)
+    msg2 = Twist2DStamped(header=None, v = 0.0, omega = 0.2)
 
     for i in range(4)
         pub.publish(msg1)
-        sleep(7)
-        pub.publish(msg2)
         sleep(4)
+        pub.publish(msg2)
+        sleep(1)
 
 
     msg3 = Twist2DStamped(header = None, v = 0, omega = 0) # stop robot

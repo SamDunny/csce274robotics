77ff6da284d3e0e0527f71b5d6b4305bcd35f99f
SamDunny <samdunny.edu11@gmail.com>
2020-11-05 20:00:34 -0500

    first draft square.py

---
+++ packages/project3/src/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import rospy
+from duckietown_msgs.msg import Twist2DStamped
+from time import sleep
+
+def square():
+    pub = rospy.Publisher('car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
+    rospy.init_node('project_3_square')
+    msg1 = Twist2DStamped(header=None, v = 0.3, omega = 0.0)
+    msg2 = Twist2DStamped(header=None, v = 0.0, omega = 0.5)
+
+    # first side and turn
+   # pub.publish(msg1)
+   # sleep(7)
+   # pub.publish(msg2)
+   # sleep(4)
+
+    # second side and turn
+   # pub.publish(msg1)
+   # sleep(7)
+   # pub.publish(msg2)
+   # sleep(4)
+
+    # third side and turn
+   # pub.publish(msg1)
+   # sleep(7)
+   # pub.publish(msg2)
+   # sleep(4)
+
+    for i in range(4)
+        pub.publish(msg1)
+        sleep(7)
+        pub.publish(msg2)
+        sleep(4)
+
+
+    msg3 = Twist2DStamped(header = None, v = 0, omega = 0) # stop robot
+    pub.publish(msg3)
+    
+    
+if __name__ == '__main__':
+    try:
+        square()
+    except rospy.ROSInterruptException:
+        pass
+

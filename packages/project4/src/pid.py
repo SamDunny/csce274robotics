5a137f85ae30102904bfe90d99e21428a8166fb2
SamDunny <samdunny.edu11@gmail.com>
2020-11-20 17:55:27 -0500

    more progress

--- packages/project4/src/pid.py
+++ packages/project4/src/pid.py
@@ -2,14 +2,14 @@
 
 import rospy
 from duckietown_msgs.msg import LanePose
+from duckietown_msgs.msg import Twist2DStamped
 
 class Project4:
 
     def __init__(self):
         rospy.Subscriber("lane_filter_node/lane_pose", LanePose, self.callback)
-        self.pub1 = rospy.Publisher("lane_controller_node", LanePose, queue_size=10)
-        self.pub2 = rospy.Publisher("lane_pose_visualizer_node", LanePose, queue_size = 10)    
-        
+        self.pub = rospy.Publisher("car_cmd_switch_node/cmd", Twisted2DStamped, queue_size=10)
+
         # starting off with 0 previous error and 0 for the integral value
         self.prevErr_d = 0
         self.integral_d = 0
@@ -26,11 +26,11 @@ class Project4:
 
 
     # gets called everytime lan_pose publishes new data
-    def callback(self, data):
+    def callback(data):
         # calling pid_d for d variable
-        pid_d(self, data.d)
+        pid_d(data.d)
         # calling pid_phi for phi variable
-        pid_phi(self, data.phi)
+        pid_phi(data.phi)
 
 
     # pid controller for d (variable to stay in center of the lane)
@@ -40,9 +40,8 @@ class Project4:
         derivative = error - self.prevErr_d
         output = (self.Kp * error) + (self.Ki * self.integral_d) + (Kd * derivative_d)
         self.prevErr_d = error
-        msg = LanePose(header = None, d = output)
-        self.pub1.publish(msg)
-        self.pub2.publish(msg)
+        msg = Twist2DStamped(header = None, v = output, omega = 0)
+        self.pub.publish(msg)
 
 
     # pid controller for phi (variable to remain parallel in-between the lines)
@@ -52,9 +51,8 @@ class Project4:
         derivative = error - self.prevErr_phi
         output = (self.Kp * error) + (self.Ki * self.integral_phi) + (Kd * derivative_phi)
         self.prevErr_phi = error
-        msg = LanePose(header = None, phi = output)
-        self.pub1.publish(msg)
-        self.pub2.publish(msg)
+        msg = Twist2DStamped(header = None, v = 0, omega = output)
+        self.pub.publish(msg)
     
     
 if __name__ == '__main__':
